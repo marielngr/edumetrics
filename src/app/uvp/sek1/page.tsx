70980001e2f43992d705ab5c4baf2560cc8864d4
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-06-11 11:24:50 +0200

    add some flexbox-basics etc

---
+++ src/app/uvp/sek1/page.tsx
@@ -0,0 +1,21 @@
+import styles from "./page.module.scss";
+
+export default function UvpS1() {
+  console.log(styles);
+  console.log("___");
+  return (
+    <div className={styles.container}>
+      <section className={styles.sidebarLeft}>
+        <div className={styles.sidebarLeft__content}>hallo</div>
+        <div className={styles.sidebarLeft__content}>hallo</div>
+      </section>
+      <section className={styles.tablesheet}>
+        <h2 className={styles.tablesheet__h2}>Überschrift what ever</h2>
+        <div className={styles.tablesheet__Wrapper}>
+          <div className={styles.tableContent}>hallo </div>
+          <div className={styles.tableContent}>hallo</div>
+        </div>
+      </section>
+    </div>
+  );
+}
