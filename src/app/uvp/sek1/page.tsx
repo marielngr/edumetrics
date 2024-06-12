acf727457457a09005f1116f4e04c70310053d9b
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-06-12 08:32:50 +0200

    add TableHeader base

--- src/app/uvp/sek1/page.tsx
+++ src/app/uvp/sek1/page.tsx
@@ -1,8 +1,27 @@
 import styles from "./page.module.scss";
+import { klassen } from "@/initial_data";
+import { Klasse } from "@/model";
 
-export default function UvpS1() {
-  console.log(styles);
-  console.log("___");
+type TableHeaderProps = {
+  klassen: Klasse[];
+};
+
+function TableHeader({ klassen }: TableHeaderProps) {
+  //für jede Klasse ein div (Spalte) erstellen und darin den Namen anzeigen
+  const liste = klassen.map((klasse) => {
+    return (
+      <div key={klasse.id} className={styles.tableContent}>
+        {klasse.name}
+      </div>
+    );
+  });
+
+  return <h3 className={styles.tableContent}>{liste}</h3>;
+}
+
+export default function VerteilungsplanS1() {
+  console.log("klassen:", klassen);
+
   return (
     <div className={styles.container}>
       <section className={styles.sidebarLeft}>
@@ -12,6 +31,7 @@ export default function UvpS1() {
       <section className={styles.tablesheet}>
         <h2 className={styles.tablesheet__h2}>Überschrift whatever</h2>
         <div className={styles.tablesheet__Wrapper}>
+          <TableHeader klassen={klassen} />
           <div className={styles.tableContent}>hallo </div>
           <div className={styles.tableContent}>hallo</div>
         </div>
