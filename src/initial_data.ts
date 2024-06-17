22e1a64d9c38a27bfc411d4df74cee6a80fbfdad
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-06-17 11:17:20 +0200

    adjust models and initialData

--- src/initial_data.ts
+++ src/initial_data.ts
@@ -1,5 +1,51 @@
-import { Lehrer, Zeitabschnitt, Fach, Vorgabe, Klasse } from "./model";
-export { lehrerListe, klassen, faecher, zeitabschnitte, vorgaben };
+import {
+  Lehrer,
+  Schuljahr,
+  Fach,
+  Benotung,
+  Klasse,
+  Organisationsform,
+} from "./model";
+export { lehrerListe, klassen, faecher, organisationsform, noten, schuljahre };
+
+const organisationsform: Organisationsform[] = [
+  {
+    id: "org-Jg5",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 5,
+    periodenUndKlausuren: [3, 3],
+  },
+  {
+    id: "org-Jg6",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 6,
+    periodenUndKlausuren: [3, 3],
+  },
+  {
+    id: "org-Jg7",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 7,
+    periodenUndKlausuren: [3, 3],
+  },
+  {
+    id: "org-Jg8",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 8,
+    periodenUndKlausuren: [3, 3],
+  },
+  {
+    id: "org-Jg9",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 9,
+    periodenUndKlausuren: [3, 3],
+  },
+  {
+    id: "org-Jg10",
+    gueltigVon: "2021/22",
+    jahrgangsstufe: 10,
+    periodenUndKlausuren: [3, 3],
+  },
+];
 
 const lehrerListe: Lehrer[] = [
   {
@@ -24,28 +70,64 @@ const lehrerListe: Lehrer[] = [
 
 const klassen: Klasse[] = [
   {
-    id: "klasse5a",
-    name: "5A",
-    jahrgang: 5,
-    lehrerId: "lehrer1",
+    id: "2021-22-5a",
+    kuerzel: "a",
+    eingangsSchuljahr: "2021/22",
+    eingangsJahrgangsstufe: 5,
   },
   {
-    id: "klasse5b",
-    name: "5B",
-    jahrgang: 5,
-    lehrerId: "lehrer2",
+    id: "2021-22-5b",
+    kuerzel: "b",
+    eingangsSchuljahr: "2021/22",
+    eingangsJahrgangsstufe: 5,
   },
   {
-    id: "klasse6a",
-    name: "6A",
-    jahrgang: 6,
-    lehrerId: "lehrer3",
+    id: "2021-22-5c",
+    kuerzel: "c",
+    eingangsSchuljahr: "2021/22",
+    eingangsJahrgangsstufe: 5,
   },
   {
-    id: "klasse6b",
-    name: "6B",
-    jahrgang: 6,
-    lehrerId: "lehrer3",
+    id: "2021-22_5abc",
+    kuerzel: "abc",
+    eingangsSchuljahr: "2021/22",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2020/21_5a",
+    kuerzel: "a",
+    eingangsSchuljahr: "2020/21",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2020/21_5b",
+    kuerzel: "b",
+    eingangsSchuljahr: "2020/21",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2020/21_5c",
+    kuerzel: "c",
+    eingangsSchuljahr: "2020/21",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2019/20_5a",
+    kuerzel: "a",
+    eingangsSchuljahr: "2019/20",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2019/20_5b",
+    kuerzel: "b",
+    eingangsSchuljahr: "2019/20",
+    eingangsJahrgangsstufe: 5,
+  },
+  {
+    id: "2019/20_5c",
+    kuerzel: "c",
+    eingangsSchuljahr: "2019/20",
+    eingangsJahrgangsstufe: 5,
   },
 ];
 
@@ -72,65 +154,156 @@ const faecher: Fach[] = [
   },
 ];
 
-const zeitabschnitte: Zeitabschnitt[] = [
+const schuljahre: Schuljahr[] = [
   {
-    id: "za2023-1",
-    schuljahr: 2023,
-    halbjahr: "1",
-    version: 1,
+    id: "2019/20",
+    name: "2019/20",
+    startjahr: 2021,
+  },
+  {
+    id: "2020/21",
+    name: "2020/21",
+    startjahr: 2021,
+  },
+  {
+    id: "2021/22",
+    name: "2021/22",
+    startjahr: 2021,
+  },
+  {
+    id: "2022/23",
+    name: "2022/23",
+    startjahr: 2022,
+  },
+  {
+    id: "2023/24",
+    name: "2023/24",
+    startjahr: 2023,
   },
 ];
 
-const vorgaben: Vorgabe[] = [
+const noten: Benotung[] = [
   {
-    id: "vorgabe15",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse5a"],
+    id: "2021-22-5a-1_1",
+    schuljahrId: "2021/22",
+    klasseId: "2021-22-5a",
+    lehrerId: "lehrer1",
+    periodenNummer: 1,
+    laufendeNummer: 1,
+    note: 2.1,
     fachId: "deutsch",
-    stundenanzahl: 4,
   },
   {
-    id: "vorgabe15b-de",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse5b"],
+    id: "2021-22-5a-1_2",
+    schuljahrId: "2021/22",
+    klasseId: "2021-22-5a",
+    lehrerId: "lehrer1",
+    periodenNummer: 1,
+    laufendeNummer: 2,
+    note: 2.2,
     fachId: "deutsch",
-    stundenanzahl: 4,
   },
   {
-    id: "vorgabe2",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse5b"],
+    id: "2021-22-5a-1_3",
+    schuljahrId: "2021/22",
+    klasseId: "2021-22-5a",
+    lehrerId: "lehrer1",
+    periodenNummer: 1,
+    laufendeNummer: 3,
+    note: 2.3,
+    fachId: "deutsch",
+  },
+  {
+    id: "2020-21-5a-1_1",
+    schuljahrId: "2020/21",
     fachId: "englisch",
-    stundenanzahl: 3,
+    lehrerId: "lehrer3",
+    periodenNummer: 1,
+    laufendeNummer: 1,
+    klasseId: "5a",
+    note: 1.2,
   },
   {
-    id: "vorgabe25a-e",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse5a"],
+    id: "2019-20-5a-1_2",
+    schuljahrId: "2019/20",
     fachId: "englisch",
-    stundenanzahl: 3,
-  },
-  // Teilen der Klassen, klassenuebergreifender Unterricht
-  {
-    id: "vorgabe3",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse6a", "klasse6b"],
-    fachId: "religion",
-    stundenanzahl: 2,
-  },
-  // Teamteaching: Zwei Eintraege fuer die gleiche Klasse
-  {
-    id: "vorgabe4-1",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse6a"],
-    fachId: "deutsch",
-    stundenanzahl: 3,
+    lehrerId: "lehrer3",
+    periodenNummer: 1,
+    laufendeNummer: 2,
+    klasseId: "5a",
+    note: 1.3,
   },
   {
-    id: "vorgabe4-2",
-    zeitabschnittId: "za2023-1",
-    klassenIds: ["klasse6a"],
-    fachId: "deutsch",
-    stundenanzahl: 3,
+    id: "2019-20-5a-1_2",
+    schuljahrId: "2019/20",
+    fachId: "englisch",
+    lehrerId: "lehrer3",
+    periodenNummer: 1,
+    laufendeNummer: 2,
+    klasseId: "5a",
+    note: 1.3,
   },
 ];
+
+// const zeitabschnitte: Zeitabschnitt[] = [
+//   {
+//     id: "za2023-1",
+//     schuljahr: 2023,
+//     halbjahr: "1",
+//     version: 1,
+//   },
+// ];
+
+// const vorgaben: Vorgabe[] = [
+//   {
+//     id: "vorgabe15",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse5a"],
+//     fachId: "deutsch",
+//     stundenanzahl: 4,
+//   },
+//   {
+//     id: "vorgabe15b-de",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse5b"],
+//     fachId: "deutsch",
+//     stundenanzahl: 4,
+//   },
+//   {
+//     id: "vorgabe2",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse5b"],
+//     fachId: "englisch",
+//     stundenanzahl: 3,
+//   },
+//   {
+//     id: "vorgabe25a-e",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse5a"],
+//     fachId: "englisch",
+//     stundenanzahl: 3,
+//   },
+//   // Teilen der Klassen, klassenuebergreifender Unterricht
+//   {
+//     id: "vorgabe3",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse6a", "klasse6b"],
+//     fachId: "religion",
+//     stundenanzahl: 2,
+//   },
+//   // Teamteaching: Zwei Eintraege fuer die gleiche Klasse
+//   {
+//     id: "vorgabe4-1",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse6a"],
+//     fachId: "deutsch",
+//     stundenanzahl: 3,
+//   },
+//   {
+//     id: "vorgabe4-2",
+//     zeitabschnittId: "za2023-1",
+//     klassenIds: ["klasse6a"],
+//     fachId: "deutsch",
+//     stundenanzahl: 3,
+//   },
+// ];
--- src/model.ts
+++ src/model.ts
@@ -1,8 +1,13 @@
+import { machine } from "os";
+
 export type FachId = string;
 export type LehrerId = string;
 export type KlasseId = string;
 export type ZeitabschnittId = string;
 export type VorgabeId = string;
+export type SchuljahrId = string;
+export type OrganisationsformId = string;
+export type BenotungId = string;
 
 export interface Fach {
   name: string;
@@ -12,28 +17,95 @@ export interface Fach {
 export interface Lehrer {
   id: LehrerId;
   kuerzel: string;
-  pflichtstundenanzahl: number;
+  pflichtstundenanzahl?: number;
   faecherIds: FachId[];
 }
 
+/**
+ * Eine Klasse ist eine Gruppe von Schülern, die gemeinsam unterrichtet werden.  Sie hat in einem bestimmten
+ * Schuljahr neu gestartet, also z.B. 5. Klasse im Schuljahr 2021/22.
+ * Das Kuerzel dient der Unterscheridung der Klassen in A-Klasse usw., die aktuelle Bezichnung der Klasse, z.B. 6a,
+ * ergibt sich immer aus dem Eingangsschuljahr und der Eingangsjahrgangsstufe und haengt davon ab, in welchem
+ * aktuellen Schuljahr man die Klasse betrachtet.
+ * Es ist so gedacht, dass jede Klasse fuer sich ueber den gesamten Zeitraum, in dem sie existiert, eindeutig identifizierbar ist,
+ * der ja mehrere Schuljahr umfasst.  Daher ist die KlasseId eindeutig, aber die Jahrgangsstufe sind in verschiedenen Schuljahren
+ * natuerlich nicht eindeutig.
+ */
 export interface Klasse {
   id: KlasseId;
+  kuerzel: string; // z.B. a, b, c, d
+  eingangsSchuljahr: SchuljahrId; // y.z. 2021/22
+  eingangsJahrgangsstufe: number; // z.B. 5
+}
+
+// export interface Zeitabschnitt {
+//   id: ZeitabschnittId;
+//   schuljahr: number;
+//   halbjahr: string;
+//   version: number;
+// }
+
+// type ZuteilungId = string;
+
+// im Zeitabschnitt XZ (z.B. 2021/22, 1. Halbjahr) ist die Klasse 5A in dem Fach C dem Lehrer AB zugeordnet
+// interface Zuteilung {
+//   id: ZuteilungId;
+//   zeitabschnittId: ZeitabschnittId;
+//   klasseId: KlasseId;
+//   fachId: FachId;
+//   lehrerId: LehrerId;
+// }
+
+export interface Schuljahr {
+  id: SchuljahrId;
   name: string;
-  jahrgang: number;
-  lehrerId: LehrerId;
+  startjahr: number; // z.B. 2021
 }
 
-export interface Zeitabschnitt {
-  id: ZeitabschnittId;
-  schuljahr: number;
-  halbjahr: string;
-  version: number;
+/**
+ *  In der Organisationsform merken wir uns, wie die Jahrgangsstufen organisiert sind,
+ * insbesondere wie viele Unterterme es pro Schuljahr gibt (Halbjahre, Trimester, Quartale, ...)
+ * und wie viele Klausuren es pro Unterterm gibt.
+ */
+export interface Organisationsform {
+  id: OrganisationsformId;
+  /// Gueltig ab diesem Schuljahr.  In den meisten Schulen wird man das einfach fix auf das erste
+  /// Schuljahr setzen, in dem die Schule mit dem System anfaengt und muss es nie mehr aendern.
+  gueltigVon: SchuljahrId;
+  /// Gueltig bis zu diesem Schuljahr.  Wenn das nicht gesetzt ist, ist die Organisationsform
+  /// weiterhin gueltig.  Kann in der Regel offen gelassen werden, weil sich die Organisationsform
+  /// fast nie aendert.
+  gueltigBis?: SchuljahrId;
+
+  // z.B. Klasse 10
+  jahrgangsstufe: number;
+
+  // z.B. [3, 2] wuerde bedeuten, es sind zwei Halbjahre pro Schuljahr (weil das Array 2 Eintraege hat) und im 1. HJ gibt es 3 Klausuren, im 2. HJ nur noch 2 Klausuren
+  periodenUndKlausuren: number[];
 }
 
-export interface Vorgabe {
-  id: VorgabeId;
-  zeitabschnittId: ZeitabschnittId;
-  klassenIds: KlasseId[];
+// Fuer jede normale Schule muesste man wqhrscheinlich nur einmal eine allgemeine Organisationsform anlegen, die fuer alle Jahrgangsstufen, alle Gymnasien, Gesamtschulen usw. gilt,
+// weil ueberall pro Schuljahr 2 Halbjahre sind und in jedem Halbjahr 3 Klausuren geschrieben werden usw.
+
+/**
+ * Eine Benotung enthaelt fuer eine durchgefuehrte Klassenarbeit oder Klausur die Note, die die Klasse im Durchschnitt erreicht hat.
+ */
+
+export interface Benotung {
+  id: BenotungId; // zufaellig vergeben (in MongoDB waere das eine ObjectId)
+  schuljahrId: SchuljahrId;
+  klasseId: KlasseId;
+  lehrerId: LehrerId; // Der Lehrer, der die Klausur geschrieben-/abgenommen hat
+  periodenNummer: number; // z.B. 1 fuer 1. Halbjahr, 2 fuer 2. Halbjahr oder 1-4 wenn es Quartale sind usw.
+  laufendeNummer: number; // z.B. 2 fuer 2. KLausur in der obigen Periode (z.B. 1. HJ)
+  note: number; // Durchschnittsnote der Klasse
   fachId: FachId;
-  stundenanzahl: number;
 }
+
+// export interface VorgabeUnterrichtsverteilung {
+//   id: VorgabeId;
+//   zeitabschnittId: ZeitabschnittId;
+//   klassenIds: KlasseId[];
+//   fachId: FachId;
+//   stundenanzahl: number;
+// }
