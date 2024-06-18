e53ea8e26dc3394b03df8b07a8684d1758eb4023
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-06-18 12:15:02 +0200

    Schuljahre auslesen

--- src/app/monitoring/page.tsx
+++ src/app/monitoring/page.tsx
@@ -1,16 +1,11 @@
-import fs from "fs";
-import { promisify } from "util";
-import { useEffect, useState } from "react";
-
 async function ladeCsvDaten(filePath: string): Promise<string[][]> {
   try {
     const response = await fetch(filePath);
     const fileContent = await response.text();
     const lines = fileContent.split("\n");
     const result = lines.map((line) => line.split(";"));
-    console.log("CSV-Daten:", result);
+    // console.log("CSV-Daten:", result);
     return result;
-    return [];
   } catch (error) {
     console.error("Fehler beim Einlesen der CSV-Datei:", error);
     throw error;
@@ -19,5 +14,23 @@ async function ladeCsvDaten(filePath: string): Promise<string[][]> {
 
 export default async function Monitoring() {
   const csv = await ladeCsvDaten("http://localhost:3000/daten.csv");
+  // // erste Zeile enthält die Spaltenüberschriften, weg damit
+  csv.shift();
+
+  // Schuljahre auslesen
+  const schuljahre = csv.map((zeile) => zeile[1]).filter((sj) => sj);
+  //duplicate rausfiltern
+  const uniqueSchuljahre = Array.from(new Set(schuljahre));
+  const Schuljahre = uniqueSchuljahre.map((sj) => {
+    let startjahr = sj.slice(0, 4);
+    return {
+      id: sj,
+      name: sj,
+      startjahr: startjahr,
+    };
+  });
+  // console.log("unique Schuljahre:", uniqueSchuljahre),
+  console.log("Array of Schuljahre:", Schuljahre);
+
   return <div>Monitoring</div>;
 }
