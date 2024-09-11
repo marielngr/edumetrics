bd2c1ea455422760359f3736975167993d5d31e3
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-09-11 10:39:17 +0200

    removal + renaming of nonsense

--- src/app/uvp/sek1/layout.tsx
+++ src/app/uvp/sek1/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./page.module.scss";
 
-export default function MonitoringLayout({
+export default function UvpS1Layout({
   children,
 }: {
   children: React.ReactNode;
