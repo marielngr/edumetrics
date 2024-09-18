6596b22217522b48fea255e2f5b62ab9e7f1457b
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-09-18 12:00:32 +0200

    TableRow als Komponente ausgelagert

--- src/app/monitoring/page.tsx
+++ src/app/monitoring/page.tsx
@@ -1,84 +1,14 @@
 import { entferneDuplikate, ladeDaten } from "@/data";
 import {
   Benotung,
-  Fach,
-  Klasse,
-  Schuljahr,
   KlasseFachSchuljahrId,
   klasseFachSchuljahrSindGleich,
   klasseFachSchuljahrCompare,
 } from "@/model";
 import TableHeader from "@/components/TableHeader/TableHeader";
 import styles from "./page.module.scss";
+import TableRow from "@/components/TableRow/TableRow";
 
-type TableRowProps = {
-  zeile: KlasseFachSchuljahrId;
-  zeilennr: number;
-  klasse: Klasse;
-  jahrgang: number;
-  benotung: Benotung[];
-};
-
-function TableRow({
-  zeile,
-  zeilennr,
-  klasse,
-  jahrgang,
-  benotung,
-}: TableRowProps) {
-  const anzuzeigendeLeistungsabschnitte = [
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-  ];
-
-  function noteFuerPeriodeUndKlausur(
-    periodenNummer: number,
-    laufendeNummer: number
-  ) {
-    const note = benotung.find(
-      (note) =>
-        note.periodenNummer === periodenNummer &&
-        note.laufendeNummer === laufendeNummer
-    );
-
-    if (!note) {
-      return <div className={styles.tableCell}>-</div>;
-    }
-
-    return (
-      <div className={styles.tableCell}>
-        {note.periodenNummer}-{note.laufendeNummer}: Ø{note.note}/
-        {note.lehrerId}
-      </div>
-    );
-  }
-
-  const halbjahresdurchschnitte = [
-    berechneHalbjahresdurchschnitt(benotung, 1),
-    berechneHalbjahresdurchschnitt(benotung, 2),
-  ];
-
-  return (
-    <div className={styles.tableRow}>
-      <div className={styles.tableCell}>{zeilennr}</div>
-      <div className={styles.tableCell}>{zeile.klasseId}</div>
-      <div className={styles.tableCell}>{zeile.schuljahrId}</div>
-      <div className={styles.tableCell}>
-        {jahrgang} {klasse.kuerzel}
-      </div>
-      <div className={styles.tableCell}>{zeile.fachId}</div>
-      {anzuzeigendeLeistungsabschnitte.map((interesse) =>
-        noteFuerPeriodeUndKlausur(interesse[0], interesse[1])
-      )}
-      <div className={styles.tableCell}>Ø{halbjahresdurchschnitte[0]}</div>
-      <div className={styles.tableCell}>Ø{halbjahresdurchschnitte[1]}</div>
-    </div>
-  );
-}
 //to do: in utils auslagern
 function findNotenFuerKlasseFachSchuljahr(
   notenArray: Benotung[],
@@ -105,30 +35,6 @@ function findNotenFuerKlasseFachSchuljahr(
     return [];
   }
   return notenProZeile;
-}
-
-/**
- * Halbjahresdurchschnitt berechnen und ausgeben
- * Kommazahlen auf 2 Stellen runden
- */
-function berechneHalbjahresdurchschnitt(
-  notenProKlasseFachSchuljahr: Benotung[],
-  periodenNummer: number
-) {
-  const notenProHalbjahr = notenProKlasseFachSchuljahr.filter(
-    (note) => note.periodenNummer === periodenNummer
-  );
-
-  if (notenProHalbjahr.length === 0) {
-    return null;
-  }
-
-  let durchschnitt =
-    notenProHalbjahr.reduce((acc, note) => acc + note.note, 0) /
-    notenProHalbjahr.length;
-
-  durchschnitt = Math.round(durchschnitt * 100) / 100;
-  return durchschnitt;
 }
 
 export default async function Monitoring() {
